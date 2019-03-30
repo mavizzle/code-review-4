132a61b296dc49bad65b30870ead52f9d8c36cb4
Lex Wilkins <alexwilkins99@yahoo.com>
2019-03-30 16:46:26 -0700

    create html structure to display input fields for pizza size and topping selections, UI logic for submitting order that displays to console

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,35 @@
 
+// business logic for Pizza constructors
+
+function Order() {
+  this.orders = [];
+}
+
+var newOrder = new Order();
+
+function Pizza(size, toppings) {
+  this.size = size;
+  this.toppings = toppings;
+  this.checked = 0;
+  this.toppingDisplay = '';
+}
+
+
+// UI logic
+
+$(document).ready(function(){
+
+  $('form#orderform').submit(function(event){
+    event.preventDefault();
+
+    var numChecks = 0;
+    var toppings = [];
+    $('input:checkbox:checked').each(function(){
+      toppings.push(this.value);
+      numChecks ++;
+    });
+    var pizzaSize = $('#sizeinput').val();
+    // console.log(`${numChecks} checked boxes, toppings are ${toppings}, size is ${pizzaSize}`);
+  });
+
+});
